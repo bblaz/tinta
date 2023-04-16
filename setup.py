c0842160f704ffec5d59c97eacc73466c10eaa3d
Blaž Bregar <blaz@vexo.systems>
2023-04-16 23:00:23 +0200

    Correct version flask_babel

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ else:
             break
 if local_version:
     version += '+' + '.'.join(local_version)
-requires = ['flask_tryton', 'flask', 'flask_babel']
+requires = ['flask_tryton', 'flask', 'flask_babel<=2.0.0']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
